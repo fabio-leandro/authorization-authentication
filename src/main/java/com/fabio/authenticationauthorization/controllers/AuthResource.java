410ac9847f90cc7fb1b3d8f8906b3ecd994db223
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-06-14 17:22:33 -0300

    Refresh Token

---
+++ src/main/java/com/fabio/authenticationauthorization/controllers/AuthResource.java
@@ -0,0 +1,29 @@
+package com.fabio.authenticationauthorization.controllers;
+
+import com.fabio.authenticationauthorization.security.JWTUtil;
+import com.fabio.authenticationauthorization.security.UserSS;
+import com.fabio.authenticationauthorization.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
